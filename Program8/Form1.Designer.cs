1821c18deed3602fdca3f11697589de4ad7eab36
Alex <56237625+Yang2114@users.noreply.github.com>
2020-12-19 05:01:41 +0300

    Добавьте файлы проекта.

---
+++ Program8/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Program8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem13 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem14 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem1,
+            this.ToolStripMenuItem6,
+            this.ToolStripMenuItem10});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(524, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem1
+            // 
+            this.ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem2,
+            this.ToolStripMenuItem3,
+            this.ToolStripMenuItem4,
+            this.ToolStripMenuItem5});
+            this.ToolStripMenuItem1.Name = "ToolStripMenuItem1";
+            this.ToolStripMenuItem1.Size = new System.Drawing.Size(45, 20);
+            this.ToolStripMenuItem1.Text = "Файл";
+            // 
+            // ToolStripMenuItem2
+            // 
+            this.ToolStripMenuItem2.Name = "ToolStripMenuItem2";
+            this.ToolStripMenuItem2.Size = new System.Drawing.Size(174, 22);
+            this.ToolStripMenuItem2.Text = "Система координат";
+            this.ToolStripMenuItem2.Click += new System.EventHandler(this.ToolStripMenuItem2_Click);
+            // 
+            // ToolStripMenuItem3
+            // 
+            this.ToolStripMenuItem3.Name = "ToolStripMenuItem3";
+            this.ToolStripMenuItem3.Size = new System.Drawing.Size(174, 22);
+            this.ToolStripMenuItem3.Text = "Открыть ";
+            // 
+            // ToolStripMenuItem4
+            // 
+            this.ToolStripMenuItem4.Name = "ToolStripMenuItem4";
+            this.ToolStripMenuItem4.Size = new System.Drawing.Size(174, 22);
+            this.ToolStripMenuItem4.Text = "Сохранить";
+            this.ToolStripMenuItem4.Click += new System.EventHandler(this.ToolStripMenuItem4_Click);
+            // 
+            // ToolStripMenuItem5
+            // 
+            this.ToolStripMenuItem5.Name = "ToolStripMenuItem5";
+            this.ToolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem5.Text = "Выход";
+            this.ToolStripMenuItem5.Click += new System.EventHandler(this.ToolStripMenuItem5_Click_1);
+            // 
+            // ToolStripMenuItem6
+            // 
+            this.ToolStripMenuItem6.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem7,
+            this.ToolStripMenuItem8,
+            this.ToolStripMenuItem9});
+            this.ToolStripMenuItem6.Name = "ToolStripMenuItem6";
+            this.ToolStripMenuItem6.Size = new System.Drawing.Size(56, 20);
+            this.ToolStripMenuItem6.Text = "Фигура";
+            // 
+            // ToolStripMenuItem7
+            // 
+            this.ToolStripMenuItem7.Name = "ToolStripMenuItem7";
+            this.ToolStripMenuItem7.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem7.Text = "Эллипс";
+            this.ToolStripMenuItem7.Click += new System.EventHandler(this.ToolStripMenuItem7_Click);
+            // 
+            // ToolStripMenuItem8
+            // 
+            this.ToolStripMenuItem8.Name = "ToolStripMenuItem8";
+            this.ToolStripMenuItem8.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem8.Text = "Прямоугольник";
+            this.ToolStripMenuItem8.Click += new System.EventHandler(this.ToolStripMenuItem8_Click);
+            // 
+            // ToolStripMenuItem9
+            // 
+            this.ToolStripMenuItem9.Name = "ToolStripMenuItem9";
+            this.ToolStripMenuItem9.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem9.Text = "Много угольник";
+            this.ToolStripMenuItem9.Click += new System.EventHandler(this.ToolStripMenuItem9_Click_1);
+            // 
+            // ToolStripMenuItem10
+            // 
+            this.ToolStripMenuItem10.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem11,
+            this.ToolStripMenuItem12,
+            this.ToolStripMenuItem13,
+            this.ToolStripMenuItem14});
+            this.ToolStripMenuItem10.Name = "ToolStripMenuItem10";
+            this.ToolStripMenuItem10.Size = new System.Drawing.Size(61, 20);
+            this.ToolStripMenuItem10.Text = "Заливки";
+            // 
+            // ToolStripMenuItem11
+            // 
+            this.ToolStripMenuItem11.Name = "ToolStripMenuItem11";
+            this.ToolStripMenuItem11.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem11.Text = "Сплошная";
+            this.ToolStripMenuItem11.Click += new System.EventHandler(this.ToolStripMenuItem11_Click_1);
+            // 
+            // ToolStripMenuItem12
+            // 
+            this.ToolStripMenuItem12.Name = "ToolStripMenuItem12";
+            this.ToolStripMenuItem12.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem12.Text = "Узорная";
+            this.ToolStripMenuItem12.Click += new System.EventHandler(this.ToolStripMenuItem12_Click_1);
+            // 
+            // ToolStripMenuItem13
+            // 
+            this.ToolStripMenuItem13.Name = "ToolStripMenuItem13";
+            this.ToolStripMenuItem13.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem13.Text = "Текстурная";
+            this.ToolStripMenuItem13.Click += new System.EventHandler(this.ToolStripMenuItem13_Click_1);
+            // 
+            // ToolStripMenuItem14
+            // 
+            this.ToolStripMenuItem14.Name = "ToolStripMenuItem14";
+            this.ToolStripMenuItem14.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem14.Text = "Мозаика";
+            this.ToolStripMenuItem14.Click += new System.EventHandler(this.ToolStripMenuItem14_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 753);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem11;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem12;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem13;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem14;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem9;
+    }
+}
+
---
+++ Program8/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Program8
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics Graph1;
+        Bitmap image1 = new Bitmap(500, 800);
+        Bitmap image2 = new Bitmap("D:/Алёша/Учёба/ОПиБД/C#/Program8/textura.jpg");
+        Pen Pen1 = new Pen(Color.Black, 1);
+        Pen Pen2 = new Pen(Color.Brown, 2);
+        Pen Pen3 = new Pen(Color.Cyan, 4);
+        Font drawFont = new Font("Arial", 12);
+        SolidBrush Brush1 = new SolidBrush(Color.Blue);
+        SolidBrush Brush2 = new SolidBrush(Color.LightGoldenrodYellow);
+        HatchBrush hatchbrush1 = new HatchBrush(HatchStyle.SolidDiamond, Color.Gray, Color.DarkGreen);
+
+        int i = 0;
+        Point point1 = new Point(-100, -100);
+        Point point2 = new Point(-50, 50);
+        Point point3 = new Point(50, -100);
+        Point point4 = new Point(-50, -50);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.Clear(Color.White);
+            for (i = -250; i <= 250; i += 50)
+            {
+                Graph1.DrawString(Convert.ToString(i), drawFont, Brush1, i + 250, 410);
+            }
+            for (i = -400; i <= 400; i += 50)
+            {
+                Graph1.DrawString(Convert.ToString(i), drawFont, Brush1, 260, 400 - i);
+            }
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Graph1.DrawLine(Pen1, -250, 0, 250, 0);
+            Graph1.DrawLine(Pen1, 0, -400, 0, 400);
+            for (int i = -250; i <= 250; i += 50)
+            {
+                Graph1.DrawRectangle(Pen3, i, 0, 1, 1);
+            }
+            for (int i = -400; i <= 400; i += 50)
+            {
+                Graph1.DrawRectangle(Pen3, 0, i, 1, 1);
+            }
+        }
+
+        private void ToolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.DrawEllipse(Pen1, 5, 5, 180, 260);
+            Graph1.Dispose();
+        }
+
+        private void ToolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Graph1.DrawRectangle(Pen2, 5, 5, 180, 260);
+            Graph1.Dispose();
+        }
+
+        private void ToolStripMenuItem9_Click_1(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Point[] lomannays = { point1, point2, point3, point4, point1 };
+            Graph1.DrawPolygon(Pen3, lomannays);
+            Graph1.Dispose();
+        }
+
+        private void ToolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "BMP(*.bmp) | *.bmp";
+            saveFileDialog1.ShowDialog();
+            image1.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void ToolStripMenuItem11_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Brush1.Color = colorDialog1.Color;
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.FillEllipse(Brush1, 5, 5, 180, 260);
+        }
+
+        private void ToolStripMenuItem12_Click_1(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Graph1.FillRectangle(hatchbrush1, 5, 5, 180, 260);
+        }
+
+        private void ToolStripMenuItem13_Click_1(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Point[] lomannaya = { point1, point2, point3, point4, point1 };
+            TextureBrush TextureBrushl = new TextureBrush(image2);
+            Graph1.FillPolygon(TextureBrushl, lomannaya);
+        }
+
+        private void ToolStripMenuItem14_Click_1(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Point[] lomannaya = { point1, point2, point3, point4, point1 };
+            TextureBrush TextureBrushl = new TextureBrush(image2);
+            TextureBrushl.Transform = new Matrix(75.0f / 640.0f, 0.0f, 0.0f, 75.0f / 480.0f, 0.0f, 0.0f);
+            Graph1.FillPolygon(TextureBrushl, lomannaya);
+            Graph1.Dispose();
+        }
+
+        private void ToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+        }
+
+        private void ToolStripMenuItem5_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
